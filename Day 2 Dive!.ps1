4026caf3af4bff7c3dae5ea18fbec17cfb6057e3
David <david.naasheim@gmail.com>
2021-12-02 13:05:08 +0100

    switch

--- Day 2 Dive!.ps1
+++ Day 2 Dive!.ps1
@@ -2,46 +2,30 @@
 $stuff = Get-Clipboard | Where-Object { $_ }
 $stuffconv = $stuff | convertfrom-csv -Delimiter " " -Header "direction", "value"
 
-$up = $stuffconv.where({
-        $_.direction -eq "up"
-    })
 $upsum = 0
-$up.value.foreach({
-        $upsum = $upsum + [int]$_
-    })
-
-$down = $stuffconv.where({
-        $_.direction -eq "down"
-    })
 $downsum = 0
-$down.value.foreach({
-        $downsum = $downsum + [int]$_
-    })
-
-$forward = $stuffconv.where({
-        $_.direction -eq "forward"
-    })
 $forwardsum = 0
-$forward.value.foreach({
-        $forwardsum = $forwardsum + [int]$_
-    })
-
+foreach ($item in $stuffconv) {
+    switch ($item.direction) {
+        up { $upsum = [int]$upsum + [int]$item.value }
+        down { $downsum = [int]$downsum + [int]$item.value }
+        forward { $forwardsum = [int]$forwardsum + [int]$item.value }
+        Default {}
+    }
+}
 ($downsum - $upsum) * $forwardsum | Set-Clipboard
 
 
 # task 2
+$stuff = Get-Clipboard | Where-Object { $_ }
+$stuffconv = $stuff | convertfrom-csv -Delimiter " " -Header "direction", "value"
 $aim = 0
 $forward = 0
 $depth = 0
-
 foreach ($item in $stuffconv) {
     switch ($item.direction) {
-        down { 
-            $aim = $aim + [int]$item.value 
-        }
-        up { 
-            $aim = $aim - [int]$item.value 
-        }
+        up { $aim = $aim - [int]$item.value }
+        down { $aim = $aim + [int]$item.value }
         forward {
             $forward = $forward + [int]$item.value
             $depth = $depth + ([int]$item.value * $aim)
@@ -49,4 +33,4 @@ foreach ($item in $stuffconv) {
         Default {}
     }
 }
-$forward*$depth | Set-Clipboard
+$forward * $depth | Set-Clipboard
