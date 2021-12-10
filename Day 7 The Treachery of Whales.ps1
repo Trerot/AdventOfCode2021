2614f99600cbad70f02007886f4af4d68756a1c3
Trerot <david.naasheim@gmail.com>
2021-12-10 16:12:49 +0100

    err

--- Day 7 The Treachery of Whales.ps1
+++ Day 7 The Treachery of Whales.ps1
@@ -1,65 +1,89 @@
 
 $test = get-puzzleinput -day 7 -year 2021
-$crabs = New-Object -TypeName System.Collections.ArrayList
-($test -split ",").foreach({ $crabs.add($_) })
+Measure-Command -Expression {
+    $crabs = New-Object -TypeName System.Collections.ArrayList
+    $answer = 99999999999999
+    ($test -split ",").foreach({ [void]$crabs.add($_) })
+    
+    $crabarray = $crabs
+    $measurecrabs = ($crabarray | Measure-Object -AllStats)
+    $crabrange = $measurecrabs.Minimum..$measurecrabs.Maximum
+    
+    $sumarray = New-Object -TypeName System.Collections.ArrayList
+    $crabrange.foreach({
+            $crabnum = [int]$_
+            $sum = 0
+            $crabarray.ForEach({
+                    $num = [int]$_
+                    $distance = [math]::abs($crabnum - $num)
+                    $sum = $sum + $distance
+                })
+            if ($sum -lt $answer) {
+                $answer = $sum
+            }
+        })
+    $answer
+}
 
-$crabarray = $crabs
-$measurecrabs = ($crabarray | Measure-Object -AllStats)
-$crabrange = $measurecrabs.Minimum..$measurecrabs.Maximum
 
-$sumarray = New-Object -TypeName System.Collections.ArrayList
-$crabrange.foreach({
-        $crabnum = [int]$_
-        $sum = 0
-        $crabarray.ForEach({
-                $num = [int]$_
-                if ($crabnum -ge $num) {
-                    $sum = $sum + $crabnum - $num
-                }
-                if ($num -gt $crabnum) {
-                    $sum = $sum + $num - $crabnum
-                }
-            })
-        $sumarray.add($sum)
-    })
-$sumarray.sort()
-$answer = $sumarray[0]
+$test = get-puzzleinput -day 7 -year 2021
+Measure-Command -Expression {
+    $crabs = New-Object -TypeName System.Collections.ArrayList
+    $answer = 99999999999999
+    ($test -split ",").foreach({ [void]$crabs.add($_) })
+    
+    $crabarray = $crabs
+    $measurecrabs = ($crabarray | Measure-Object -AllStats)
+    $crabrange = $measurecrabs.Minimum..$measurecrabs.Maximum
+    
+    $sumarray = New-Object -TypeName System.Collections.ArrayList
+    $crabrange.foreach({
+            $crabnum = [int]$_
+            $sum = 0
+            $crabarray.ForEach({
+                    $num = [int]$_
+                    $distance = [math]::abs($crabnum - $num)
+                    $sum = $sum + ($distance * ($distance + 1) / 2)
+                })
+            if ($sum -lt $answer) {
+                $answer = $sum
+            }
+        })
+    $answer
+}
+
 
 #task 2 
-# had to do some stuff because above method was to slow.
+# had to do some stuff because above method was to slow.-
 $test = get-puzzleinput -day 7 -year 2021
-$crabs = New-Object -TypeName System.Collections.ArrayList
-($test -split ",").foreach({ $crabs.add([int]$_) })
-$test.count
-$measurement = $crabs | Measure-Object -AllStats
-$range = $measurement.Minimum..$measurement.Maximum
-$range.foreach({
-        New-Variable -name "counter_$_" -Value $([int64]0) -Force
-    })
-$crabs.sort()
+Measure-Command -Expression {
+    $answer = 114531288099
+    $crabs = New-Object -TypeName System.Collections.ArrayList
+    ($test -split ",").foreach({ [void]$crabs.add([int]$_) })
+    $crabs.Sort()
+    $range = $crabs[0]..$crabs[-1]
 
-$crabs.ForEach({
-        $item = $_
-        Set-Variable -Name "counter_$item" -Value $((Get-Variable -name "counter_$item" -ValueOnly) + 1)
-    })
-$counterhash = @{}
-$arraylist = New-Object -TypeName System.Collections.ArrayList
-(get-variable -Name counter_*).foreach({
-        $key = [int]($_.name -replace "counter_", "")
-        $value = $_.value
-        $counterhash.Add($key, $value)
-    })
-$sumarray = New-Object -TypeName System.Collections.ArrayList
-$range.foreach({
-    $crabnum = [Int64]$_
-    $sum = [int64]0
-    $counterhash.Keys.ForEach({
-        $crabkey = $_
-        $crabamount = $counterhash.$_
-        $distance = [math]::abs($crabnum - $crabkey)
-        $sum = $sum + (($distance*($distance+1)/2)*$crabamount)
+    $crabgroup = $crabs | Group-Object
+
+    $counterhash = @{}
+    $crabgroup.foreach({
+            $counterhash.Add([int]$_.name, [int]$_.count)
         })
-    $sumarray.add($sum)
-})
-$sumarray.sort()
-$answer = $sumarray[0]
+    }
+
+    $range.foreach({
+            $crabnum = [Int]$_
+            $sum = [int]0
+            $counterhash.Keys.ForEach({
+                    $crabkey = [int]$_
+                    $crabamount = $counterhash.$_
+                    $distance = [math]::abs($crabnum - $crabkey)
+                    $sum = $sum + (($distance * ($distance + 1) / 2) *$crabamount)
+                })
+            if ($sum -lt $answer) {
+                $answer = $sum
+            }
+        })
+    
+}
+$answer
